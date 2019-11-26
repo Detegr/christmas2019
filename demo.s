03d213fa580071e27d7454e197288d18701fb5fc
Antti Keränen <detegr@gmail.com>
2019-11-26 07:08:27 +0200

    Refactor & add comments

--- demo.s
+++ demo.s
@@ -1,52 +1,4 @@
-snowflake .macro
-  asl $d019 ; ack interrupt (re-enable it)
-  ldy #$FF
-
-\1_prevsnow:
-  sty \2
-+ ldx \2 - $100
-  lda #$FE
-\1_snow:
-  sta \2
-
-  txa
-  beq +
-
-  lda \1_snow+1
-  sta \1_prevsnow+1
-  lda \1_snow+2
-  sta \1_prevsnow+2
-
-  inc \1_snow+1
-  dex
-  stx \2 - $100
-  jmp \1_out
-
-+ ldx #$7
-  stx \2 - $100
-
-  lda \1_snow+1
-  sta \1_prevsnow+1
-  clc
-  adc #$39
-  sta \1_snow+1
-  bcc +
-  inc \1_snow+2
-
-+ inc \1_snow+2
-  lda \1_snow+2
-  cmp #$40
-  beq \1_reset
-  cmp #$41
-  beq \1_reset
-  jmp \1_out
-\1_reset:
-  lda #>\2
-  sta \1_snow+2
-  lda #<\2
-  sta \1_snow+1
-\1_out:
-.endm
+.include "snowflake.s"
 
   * = $8000
 
@@ -69,77 +21,40 @@ snowflake .macro
   sta $d011 ; bitmap mode, raster interrupt high bit
   sty $d012 ; set raster interrupt
 
+  ; Set bitmap ram to be at 0x2000 - 0x3FFF
   lda #%11101
   sta $d018
 
+  ; Load the snowflake interrupt routine
   lda #<snowisr
   ldy #>snowisr
   sta $314
   sty $315
-
-  ; End of sid playback initialization
 
   ; Clear screen
   lda #$00
   sta $d020 ; Set border and screen background to black
   sta $d021
 
-  tax
-clr:
-  lda #$01
-  sta $400,x
-  sta $500,x
-  sta $600,x
-  sta $700,x
-  lda #$FF
-  sta $2000,x
-  sta $2100,x
-  sta $2200,x
-  sta $2300,x
-  sta $2400,x
-  sta $2500,x
-  sta $2600,x
-  sta $2700,x
-  sta $2800,x
-  sta $2900,x
-  sta $2a00,x
-  sta $2b00,x
-  sta $2c00,x
-  sta $2d00,x
-  sta $2e00,x
-  sta $2f00,x
-  sta $3000,x
-  sta $3100,x
-  sta $3100,x
-  sta $3200,x
-  sta $3300,x
-  sta $3400,x
-  sta $3500,x
-  sta $3600,x
-  sta $3700,x
-  sta $3800,x
-  sta $3900,x
-  sta $3a00,x
-  sta $3b00,x
-  sta $3c00,x
-  sta $3d00,x
-  sta $3e00,x
-  sta $3f00,x
-  dex
-  bne clr
+  jsr clear_screen
 
+  ; Initialize snowflake counters
   lda #$7
-  sta $2000 - $100
-  sta $2008 - $100
-  sta $2020 - $100
+  #init_snowflake 0
+  #init_snowflake 1
+  #init_snowflake 2
+  #init_snowflake 4
+  #init_snowflake 6
   cli
 
   jmp *
 
 snowisr:
-  #snowflake s, $2000
-  #snowflake s2, $2008
-  #snowflake s3, $2020
+  #snowflake s,  0
+  #snowflake s2, 1
+  #snowflake s3, 2
+  #snowflake s3, 4
+  #snowflake s3, 6
 out:
   jsr set_sid_isr
   pla
@@ -181,6 +96,52 @@ set_snow_isr:
   sta $d011 ; bitmap mode, raster interrupt high bit
   sty $d012 ; set raster interrupt
   rts
+
+clear_screen:
+  ldx #$00
+- lda #$01
+  sta $400,x
+  sta $500,x
+  sta $600,x
+  sta $700,x
+  lda #$FF
+  sta $2000,x
+  sta $2100,x
+  sta $2200,x
+  sta $2300,x
+  sta $2400,x
+  sta $2500,x
+  sta $2600,x
+  sta $2700,x
+  sta $2800,x
+  sta $2900,x
+  sta $2a00,x
+  sta $2b00,x
+  sta $2c00,x
+  sta $2d00,x
+  sta $2e00,x
+  sta $2f00,x
+  sta $3000,x
+  sta $3100,x
+  sta $3100,x
+  sta $3200,x
+  sta $3300,x
+  sta $3400,x
+  sta $3500,x
+  sta $3600,x
+  sta $3700,x
+  sta $3800,x
+  sta $3900,x
+  sta $3a00,x
+  sta $3b00,x
+  sta $3c00,x
+  sta $3d00,x
+  sta $3e00,x
+  sta $3f00,x
+  dex
+  bne -
+  rts
+
 
 * = $1000
   music .binary "Nantco_Bakker-Christmas_Medley.sid",126
---
+++ snowflake.s
@@ -0,0 +1,84 @@
+counter_offset_from_screen_pos = $100
+
+init_snowflake .macro
+  sta $2000 + ($8 * \1) - counter_offset_from_screen_pos
+.endm
+
+scrnmem = $2000
+
+bitmap_ram_pos .function i
+.endf scrnmem + ($8 * i)
+
+snowflake_counter_pos .function i
+.endf bitmap_ram_pos(i) - counter_offset_from_screen_pos
+
+snowflake .macro
+  asl $d019 ; ack interrupt (re-enable it)
+  ; Load empty pattern to Y register
+  ; This is used to clear the previously used pixel
+  ldy #$FF
+
+\1_prevsnow:
+  ; Clear up previous snowflake position
+  sty bitmap_ram_pos(\2)
+  ; Load X counter from memory location
++ ldx snowflake_counter_pos(\2)
+  ; Load snowflake pattern (one pixel set)
+  lda #$FE
+\1_snow:
+  ; Write to current snowflake position
+  sta bitmap_ram_pos(\2)
+
+  ; Move snowflake counter to accumulator
+  txa
+  ; Loop 8 times to correctly animate the
+  ; 8x8 cell. If the counter is zero, jump
+  ; to the next 8x8 cell.
+  beq +
+
+  ; Copy the current snowflake location to
+  ; prevsnow address
+  lda \1_snow+1
+  sta \1_prevsnow+1
+  lda \1_snow+2
+  sta \1_prevsnow+2
+
+  ; Advance the snowflake one pixel down
+  inc \1_snow+1
+  dex
+  ; Decrement snowflake counter
+  stx snowflake_counter_pos(\2)
+  jmp \1_out
+
+  ; Reload snowflake counter
++ ldx #$7
+  stx snowflake_counter_pos(\2)
+
+  ; Move the snowflake to next 8x8 cell
+  ; Start by incrementing low nibble by 0x39
+  lda \1_snow+1
+  sta \1_prevsnow+1
+  clc
+  adc #$39
+  sta \1_snow+1
+  bcc +
+  ; Increment the high nibble if snow+1 overflows
+  inc \1_snow+2
+
+  ; Increment high nibble, reset if it reaches 0x40
+  ; as 0x3FFF is the end of bitmap ram
++ inc \1_snow+2
+  lda \1_snow+2
+  cmp #$40
+  beq \1_reset
+  cmp #$41
+  beq \1_reset
+  jmp \1_out
+\1_reset:
+  ; Reset snowflake position to the initial position
+  lda #>(bitmap_ram_pos(\2))
+  sta \1_snow+2
+  lda #<(bitmap_ram_pos(\2))
+  sta \1_snow+1
+\1_out:
+.endm
